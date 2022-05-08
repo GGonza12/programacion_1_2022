304191789d5f42fffa6640e9405f382c69704c5e
Gonzalo Gomez <gonza12ariel@gmail.com>
2022-05-07 22:37:25 -0300

    Primera parte terminada.

---
+++ clase_6/clase_6_tp_primera_parte_ejercicio_1.java
@@ -0,0 +1,27 @@
+package clase_6;
+/* Escribir un diseño de programa que mientras que el usuario
+ingrese un número distinto de 0, pida ingresar otro numero y
+lo imprima. */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class clase_6_tp_primera_parte_ejercicio_1 {
+
+	public static void main(String[] args) {
+		int numero;
+		try {
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Ingrese un numero");
+			numero = Integer.valueOf(entrada.readLine());
+			while (numero != 0) {
+				System.out.println(numero);
+				System.out.println("Ingrese un numero o ingrese 0 para salir.");
+				numero = Integer.valueOf(entrada.readLine());
+				
+			}
+		}
+		catch(Exception exc) {
+			System.out.println(exc);
+		}
+	}
+
+}
---
+++ clase_6/clase_6_tp_primera_parte_ejercicio_2.java
@@ -0,0 +1,35 @@
+package clase_6;
+/* Escribir un diseño de programa que mientras que el usuario
+ingrese un caracter dígito o caracter letra minúscula,
+imprima dicho caracter, y si es caracter letra minúscula,
+imprima si es vocal o consonante. */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class clase_6_tp_primera_parte_ejercicio_2 {
+
+	public static void main(String[] args) {
+		char caracter;
+		try {
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Ingrese un caracter.");
+			caracter = entrada.readLine().charAt(0);
+			while (((caracter>='0') && (caracter<='9')) || ((caracter>='a')&&(caracter<='z')) ) {
+				System.out.println("El caracter es: " + caracter);
+				if(((caracter>='0') && (caracter<='9'))!=true) {
+					if ((caracter=='a')||(caracter=='e')||(caracter=='i')||(caracter=='o')||(caracter=='u')) {
+						System.out.println("El caracter es vocal");
+					}
+					else {
+						System.out.println("El caracter es consonante ");
+					}
+				}
+				System.out.println("Ingrese un caracter.");
+				caracter = entrada.readLine().charAt(0);
+			}
+		}
+		catch(Exception exc) {
+			System.out.println(exc);
+		}
+	}
+
+}
