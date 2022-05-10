ed2508f3e184447cf4fbfebf540e4ae0ff491448
Gonzalo Gomez <gonza12ariel@gmail.com>
2022-05-09 23:36:24 -0300

    Segunda parte terminada.

---
+++ clase_6/clase_6_tp_segunda_parte_ejercicio_3.java
@@ -0,0 +1,35 @@
+package clase_6;
+/* Escribir un programa que mientras el usuario cargue desde
+teclado un caracter letra minúscula, llame a un método
+que imprime por pantalla la tabla de multiplicar de 9. */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class clase_6_tp_segunda_parte_ejercicio_3 {
+
+	public static final int MULT = 9;
+	public static void main(String[] args) {
+		char caracter;
+		try {
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Ingrese un caracter");
+			caracter = entrada.readLine().charAt(0);
+			while ((caracter>='a')&&(caracter<='z')) {
+				imprimir_tabla();
+				System.out.println("Ingrese un caracter");
+				caracter = entrada.readLine().charAt(0);
+			}
+			
+			
+		}
+		catch(Exception exc) {
+			System.out.println(exc);
+		}
+		
+	}
+	public static void imprimir_tabla() {
+		for (int num=1;num<=10;num++) {
+			System.out.println("9 * " + num +" = " + (num*MULT));
+		}
+	}
+
+}
---
+++ clase_6/clase_6_tp_segunda_parte_ejercicio_4.java
@@ -0,0 +1,37 @@
+package clase_6;
+/* Escribir un programa que mientras el usuario cargue desde
+teclado un numero entero distinto de 0, imprima por
+pantalla la suma que se obtiene de invocar un método
+que calcula la sumatoria de los primeros 200 números
+naturales (son números enteros en 1 y 200). */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class clase_6_tp_segunda_parte_ejercicio_4 {
+
+	public static void main(String[] args) {
+		int num,resultado;
+		try {
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Ingrese un numero.");
+			num = Integer.valueOf(entrada.readLine());
+			while (num!=0) {
+				resultado = sumatoria();
+				System.out.println(resultado);
+				System.out.println("Ingrese un numero.");
+				num = Integer.valueOf(entrada.readLine());
+			}
+		}
+		catch(Exception exc) {
+			System.out.println(exc);
+		}
+
+}
+	public static int sumatoria() {
+		int suma=0;
+		for(int i=1;i<=200;i++) {
+			suma+=i;
+		}
+		return suma;
+	}
+	
+}
